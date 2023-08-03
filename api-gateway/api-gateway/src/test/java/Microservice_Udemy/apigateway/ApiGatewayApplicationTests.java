9d8451af2e42d915f483b5b2d725bfe782de4ea5
HussarTheBoyar <102010596+HussarTheBoyar@users.noreply.github.com>
2023-08-03 16:00:45 +0700

    Update application.properties file

---
+++ api-gateway/api-gateway/src/test/java/Microservice_Udemy/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservice_Udemy.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ currency-conversion-service/currency-conversion-service/src/test/java/Microservice_Udemy/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservice_Udemy.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
